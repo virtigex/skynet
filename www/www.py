f282563d1060d3c15b25e388e64bf2a09e547e13
Steve Austin <kn6orm@gmail.com>
2026-04-27 01:44:49 -0700

    flask

--- www/www.py
+++ www/www.py
@@ -1,2 +1,10 @@
-print('hello virtigex')
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
